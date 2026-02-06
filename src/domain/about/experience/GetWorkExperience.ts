9f582a2144394e34f2ccedfdf115c26627d2b7de
Youri Scheepers <youri.scheepers@framna.com>
2026-02-06 22:40:31 +0100

    Fix: Correct ordering of work experience

--- src/domain/about/experience/GetWorkExperience.ts
+++ src/domain/about/experience/GetWorkExperience.ts
@@ -6,8 +6,7 @@ export class GetWorkExperience {
     private repository = new ExperienceRepository()
 
     public invoke(): WorkExperience[] {
-        // TODO Fix this sorting not working
         return this.repository.getExperience()
-            .sort((a, b) => a.startDate.getUTCMilliseconds() - b.startDate.getUTCMilliseconds())
+            .sort((a, b) => b.startDate.getTime() - a.startDate.getTime())
     }
 }
